8f0d4c203d095a191794c93f76e9c41a62770feb
Iknowcapi <jeremy.f.wall@gmail.com>
2025-10-31 00:02:24 -0500

    FIX: Correct API route paths

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.get('/', (req, res) => {
 });
 
 // Your Netlify functions as Express routes
-app.post('/api/generate-story', require('./netlify/functions/generate-story').handler);
-app.post('/api/generate-image', require('./netlify/functions/generate-image').handler);
-app.post('/api/generate-audio', require('./netlify/functions/generate-audio').handler);
+app.post('/api/generate-story', require('./generate-story').handler);
+app.post('/api/generate-image', require('./generate-image').handler);
+app.post('/api/generate-audio', require('./generate-audio').handler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
